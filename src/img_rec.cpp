92d97aa02088912b331d0e99f573d1fb6bc2e8c7
Irene Pérez <ireneperezsalesa@gmail.com>
2020-10-28 16:41:39 +0100

    Updated code.

--- src/img_rec.cpp
+++ src/img_rec.cpp
@@ -1,4 +1,4 @@
-// RECONSTRUCTION USING FIRST APS FRAME
+// RECONSTRUCTION - GREY BACKGROUND, USING PREVIOUS IMAGE TO ADD NEW EVENTS
 
 #include <ros/ros.h>
 #include <iostream>
@@ -13,70 +13,56 @@
 
 cv::Mat frame = cv::Mat::zeros(cv::Size(240,180), CV_8UC1);
 cv_bridge::CvImage cv_image;
+cv_bridge::CvImage last;
 
 int counter = 1;
-bool got_first_image = false; // to take just the first frame from image_raw 
-
-
-void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
-{
-	if (got_first_image == false)
-	{
-		cv_bridge::CvImagePtr cv_ptr;
-		try
-		{
-			cv_ptr = cv_bridge::toCvCopy(msg, "mono8");
-		}
-		catch (cv_bridge::Exception& e)
-		{
-			ROS_ERROR("cv_bridge exception: %s", e.what());
-			return;
-		}
-	
-		// store the cv::Mat image in "frame"
-		frame = cv_ptr->image;
-		
-		got_first_image = true;
-
-		std::cout << "Got first frame: " << got_first_image << std::endl; 
-	}
-}
+bool got_first_image = false; 
 
 void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg)
 {
-	ROS_INFO("I got here.");
+	if (got_first_image == false)
+	{	
+		cv_image.image = cv::Mat(msg->height, msg->width, CV_8UC1);
+        	cv_image.image = cv::Scalar(128);
+		cv_image.encoding="mono8";
+		got_first_image = true;
+	}
+	else {
+		cv_image.image = last.image;
+	}
+
 	for (int i = 0; i < msg->events.size(); ++i)
 	{
         	const int x = msg->events[i].x;
         	const int y = msg->events[i].y;
 
         	
-            	if (msg->events[i].polarity == true && frame.at<uint8_t>(cv::Point(x, y)) < (uint8_t)225)  
+            	if (msg->events[i].polarity == true && cv_image.image.at<uint8_t>(cv::Point(x, y)) < (uint8_t)225)  
 		{
-			frame.at<uint8_t>(cv::Point(x, y))+=(uint8_t)30; // how much should the pixel value change for each event?
+			cv_image.image.at<uint8_t>(cv::Point(x, y))+=(uint8_t)30; // adjust value here
 		}
 		else if (msg->events[i].polarity == true) 
 		{
-			frame.at<uint8_t>(cv::Point(x, y))=(uint8_t)255;
+			cv_image.image.at<uint8_t>(cv::Point(x, y))=(uint8_t)255;
 		}		
-		else if (msg->events[i].polarity == false && frame.at<uint8_t>(cv::Point(x, y)) > (uint8_t)30)
+		else if (msg->events[i].polarity == false && cv_image.image.at<uint8_t>(cv::Point(x, y)) > (uint8_t)30)
 		{
-			frame.at<uint8_t>(cv::Point(x, y))-=(uint8_t)30; 
+			cv_image.image.at<uint8_t>(cv::Point(x, y))-=(uint8_t)30; 
 		}
 		else 
 		{
-			frame.at<uint8_t>(cv::Point(x, y))=(uint8_t)0;
+			cv_image.image.at<uint8_t>(cv::Point(x, y))=(uint8_t)0;
 		}
 		
 	}
-	// transform from cv::Mat with cv_bridge
-	std_msgs::Header header; // empty header
-	header.seq = counter; // user defined counter
-	header.stamp = ros::Time::now(); // time
-	cv_image = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO8, frame);
 
-	counter++;
-	ROS_INFO("Added events.");
+	last.image = cv_image.image;
+
+	ros::Time last_event_time = msg->events[msg->events.size()/2].ts;
+	ros::Time current_time = ros::Time::now();
+	ros::Duration reconstruction_time = current_time - last_event_time;
+
+	std::cout << "Reconstruction time: " << reconstruction_time.toSec() << std::endl;
 }
 
 
@@ -91,12 +77,7 @@ int main(int argc, char **argv)
 
 	image_transport::ImageTransport it(n);
 
-	// subscribe to /dvs/image_raw
-	image_transport::Subscriber image_sub = it.subscribe("/dvs/image_raw", 1, imageCallback);
-	std::cout << "Subscribed to /dvs/image_raw." << std::endl;
-	ros::spinOnce();
-
-	// subscribe to /dvs/events and do callbacks (modify frame according to event polarity in the callback)
+	// subscribe to /dvs/events 
 	ros::Subscriber events_sub = n.subscribe("/dvs/events", 1, eventsCallback);
 	std::cout << "Subscribed to /dvs/events." << std::endl;
 
